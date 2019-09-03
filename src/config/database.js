747da36293b5bed0098e7b05b26cd08ba723a19a
JeanCPJustino <jeancarlospereirajustino@gmail.com>
2019-09-03 07:07:00 -0300

    commit inicial: Configuração do Projeto

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dialect: 'postgrees',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarbaer',
+  define: {
+    timestamps: true,
+    /* ESSAS CONFIGURAÇÕES DEFINEM A PADRONIZAÇÃO DE NOMENCLATURA DE CRIAÇÃO
+       DE TABELAS E COLUNAS UTILIZANDO O UNDERSCORED, OU SEJA, SE EU POSSUIR UM MODEL
+       CHAMADO DE UserGroup, O SEQUELIZE IRÁ CRIAR UMA TABELA COM O NOME user_groups
+       (CAIXA BAIXA SEPARADA POR UNDERLINES) E NÃO UserGroups COMO É O PADRÃO (CAMMEL CASE) */
+    underscored: true, // Responsável pela configuração dos nomes das tabelas
+    underscoredAll: true, // Responsável pela configuração das colunas das tabelas
+  },
+};
